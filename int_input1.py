0373a37839cade394615309bc996e56158c9daab
sarangkhim <sarang.khim@gmail.com>
2017-03-13 16:48:51 +0900

    update

--- int_input1.py
+++ int_input1.py
@@ -1,3 +1,11 @@
-def input_int():
+def input_int(msg):
+    if True:
+        return [int(i) for i in input(msg).split()]
+    else:
+        return map(int, input().split())
 
+if __name__ == "__main__":
+    n = input_int()
 
+    for i in n:
+        print(i, end=' ')
