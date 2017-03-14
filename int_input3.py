601e3f67ee780b8c35c0cf49d307412a150719a0
sarangkhim <sarang.khim@gmail.com>
2017-03-14 12:17:39 +0900

    update

--- int_input3.py
+++ int_input3.py
@@ -1,17 +1,31 @@
 import re
 
 
-def input_int():
-    msg = input()
-    m = re.sub(r'and', ' ', msg)
+def input_fmt(fm, msg=''):
+    ptn = ''
+    conv = []
 
-    if True:
-        return [int(i) for i in input(m).split()]
-    else:
-        return map(int, input().split())
+    for c in fm:
+        if c == 'i':  # r'(\d+)'
+            ptn += r'(\d+)'
+            conv.append(int)
+        if c == 'f':  # r'([\d+.])'
+            ptn += r'([\d+.])'
+            conv.append(float)
+        if c == 'b' or c == 's':  # r'(\w+)'
+            ptn += r'(\w+)'
+            conv.append(str)
+
+    print(conv)
+
+    m = input(msg)
+    pt = re.compile(ptn)
+
+    return pt.search(m)
+
 
 if __name__ == "__main__":
-    n = input_int()
+    ret = input_fmt('iibsf', 'enter num: ')
 
-    for i in n:
+    for i in ret:
         print(i, end=' ')
--- re_test.py
+++ re_test.py
@@ -1,8 +1,9 @@
 import re
 
-print(re.sub(r'\b(\d{4}-\d{4})', r'<I>\1</I>', 'copyright mincom 1985-2017'))
+#print(re.sub(r'\b(\d{4}-\d{4})', r'<I>\1</I>', 'copyright mincom 1985-2017'))
 #print(re.sub(r'-', '@', 'xyz-google.com'))
 
+print(re.findall(r'[^4-6]', '12345678'))
 #print(re.findall(r'app|w*', 'application orange banana apple'))
 
 #print(re.split(r'[:. ]+', 'apple orange:banana  .tomato'))
